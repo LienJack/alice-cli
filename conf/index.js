71f2e889c1021a9d8146e91de233797f9c1feb9a
lienjie <longyuhuhao@126.com>
2019-07-10 14:21:06 +0800

    1.保留备注版本

---
+++ conf/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  getDefaultPrompt: (context) => {
+    return [
+      {
+        name: 'projectName',
+        message: '项目的名称',
+        default: context.name
+      }, {
+        name: 'projectVersion',
+        message: '项目的版本号',
+        default: '1.0.0'
+      }, {
+        name: 'projectDescription',
+        message: '项目的简介',
+        default: `A project named ${context.name}`
+      }
+    ]
+  }
+}
