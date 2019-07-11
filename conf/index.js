e88d7b609e056f8acf49bc3d90f016774bf3fccc
lienjie <longyuhuhao@126.com>
2019-07-11 10:55:23 +0800

    1.初步完成架构

--- conf/index.js
+++ conf/index.js
@@ -15,5 +15,26 @@ module.exports = {
         default: `A project named ${context.name}`
       }
     ]
-  }
+  },
+  updateNPMPrompt: [
+    {
+      type: 'list',
+      message: '请选择更新npm的方法',
+      name: 'npmType',
+      choices:[
+        {
+          name: "npm",
+          message: 'npm'
+        },
+        {
+          name: 'cnpm',
+          message: 'cnpm'
+        },
+        {
+          name: 'yarn',
+          message: 'yarn'
+        }
+      ]
+    }
+  ]
 }
